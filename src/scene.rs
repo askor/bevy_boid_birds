1f8199bd01db68d09c94e39d68250aff076a6700
Askor <askor@users.noreply.github.com>
2023-01-29 18:26:36 +0100

    Cube and light

--- src/scene.rs
+++ src/scene.rs
@@ -6,7 +6,7 @@ pub struct ScenePlugin;
 impl Plugin for ScenePlugin {
     fn build(&self, app: &mut App) {
         app
-        .add_system_set(SystemSet::on_update(GameState::Playing).with_system(setup_scene));
+        .add_system_set(SystemSet::on_enter(GameState::Playing).with_system(setup_scene));
     }
 }
 
@@ -27,4 +27,22 @@ fn setup_scene(
         },
         Name::new("Ground")
     ));
+
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 1., 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(3.0, 4.0, -1.0),
+        ..default()
+    });
 }
