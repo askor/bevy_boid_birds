530eb2bd2e7ace04dfdcc35715572b25c6b9729a
Askor <askor@users.noreply.github.com>
2023-01-29 20:40:55 +0100

    Rotate camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,12 +1,14 @@
 use bevy::prelude::*;
-use crate::GameState;
+use crate::{GameState, actions::{Actions, self}};
 
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app
-        .add_system_set(SystemSet::on_enter(GameState::Playing).with_system(setup_camera));
+        .add_system_set(SystemSet::on_enter(GameState::Playing).with_system(setup_camera))
+        .add_system_set(SystemSet::on_update(GameState::Playing).with_system(rotate_camera))
+        ;
     }
 }
 
@@ -15,9 +17,20 @@ fn setup_camera(
 ) {
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(0., 2., -4.),
+            transform: Transform::from_xyz(0., 2., 7.),
             ..default()
         },
         Name::new("Camera"),
     ));
+}
+
+fn rotate_camera(
+    mut query: Query<&mut Transform, With<Camera>>,
+    time: Res<Time>,
+) {
+    let mut cam_transform = query.single_mut();
+    let focus = Vec3::new(0., 1., 0.);
+
+    cam_transform.translate_around(focus, Quat::from_rotation_y(time.delta_seconds()));
+    cam_transform.look_at(focus, Vec3::Y)
 }
