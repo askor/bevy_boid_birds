b7fe31b58416e637055102de4364aa3565223546
Askor <askor@users.noreply.github.com>
2023-01-31 10:10:30 +0100

    WIP Index

--- src/boids.rs
+++ src/boids.rs
@@ -42,7 +42,7 @@ fn spawn_boids (
     let mut rng = rand::thread_rng();
     
     for i in 0..1 {
-        let pos = Vec3::new(rng.gen_range(-10..10) as f32, rng.gen_range(-10..10) as f32, rng.gen_range(-10..10) as f32);
+        let pos = Vec3::new(rng.gen_range(-9..9) as f32, rng.gen_range(-9..9) as f32, rng.gen_range(-9..9) as f32);
 
         println!("Pos: {}", pos);
         println!("Calc index: {:?}", get_cell_index(pos));
@@ -74,28 +74,34 @@ fn move_boids (
     time: Res<Time>,
 ) {
     for (mut transform, entity, velocity) in boid_query.iter_mut() {
-        let prev = transform.translation;
         let focus = transform.translation - velocity.0;
         let up = transform.local_y();
         transform.look_at(focus, up);
-        transform.translation += velocity.0 * time.delta_seconds() * SPEED;
+        
+        let prev = transform.translation;
+        let new_pos = velocity.0 * time.delta_seconds() * SPEED;
 
-        let index0 = get_cell_index(prev);
-        let index1 = get_cell_index(transform.translation);
-        if index0 != index1 {
-            let vec = grid.map.get_mut(&get_key(index0)).unwrap();
-            vec.remove(
-                vec.iter().position(|x| *x == entity)
-                .expect(format!("No such entity found. Prev: {}, Current: {}", prev, transform.translation).as_str())
-            );
-            println!("New index: {:?}", index1);
-            grid.map.get_mut(&get_key(index1)).unwrap().push(entity);
-        };
+        update_grid(prev, new_pos, &mut grid, entity);
     }
 }
 
-const BOUNDS: f32 = 20.0;
-const DIMENSIONS: i32 = 2;
+fn update_grid(prev: Vec3, new_pos: Vec3, grid: &mut ResMut<GridMap>, entity: Entity) {
+    let index0 = get_cell_index(prev);
+    let index1 = get_cell_index(new_pos);
+    println!("index: {:?}, pos: {}", index1, new_pos);
+    if index0 != index1 {
+        println!("Prev: {:?}, index: {:?}", index0, index1);
+        let vec = grid.map.get_mut(&get_key(index0)).unwrap();
+        vec.remove(
+            vec.iter().position(|x| *x == entity)
+            .expect(format!("No such entity found. Prev: {}, Current: {}", prev, new_pos).as_str())
+        );
+        grid.map.get_mut(&get_key(index1)).unwrap().push(entity);
+    };
+}
+
+const BOUNDS: [Vec3; 2] = [Vec3::new(-10., -10., -10.), Vec3::new(10., 10., 10.)];
+const DIMENSIONS: [i32; 3] = [10, 10, 10];
 
 #[derive(Resource)]
 struct GridMap {
@@ -106,9 +112,9 @@ fn init_grid_map (
     mut commands: Commands
 ) {
     let mut map: HashMap<String, Vec<Entity>> = HashMap::new();
-    for x in 0..DIMENSIONS {
-        for y in 0..DIMENSIONS {
-            for z in 0..DIMENSIONS {
+    for x in 0..DIMENSIONS[0] {
+        for y in 0..DIMENSIONS[1] {
+            for z in 0..DIMENSIONS[2] {
                 let key = get_key((x, y, z));
                 map.insert(key, Vec::new());
                 println!("Index: {}:{}:{}", x, y, z);
@@ -124,13 +130,21 @@ fn get_key (index: (i32, i32,i32)) -> String {
 }
 
 fn get_cell_index (pos: Vec3) -> (i32, i32, i32) {
-    let min = -BOUNDS/2.0;
-    let max = BOUNDS/2.0;
 
-    let x = (((pos.x + max).clamp(0., BOUNDS) / BOUNDS) * DIMENSIONS as f32).floor() as i32;
-    let y = (((pos.y + max).clamp(0., BOUNDS) / BOUNDS) * DIMENSIONS as f32).floor() as i32;
-    let z = (((pos.z + max).clamp(0., BOUNDS) / BOUNDS) * DIMENSIONS as f32).floor() as i32;
-    return (x, y, z);
+    // let x = (((pos.x + max).clamp(0., BOUNDS) / BOUNDS) * DIMENSIONS as f32).floor() as i32;
+    // let y = (((pos.y + max).clamp(0., BOUNDS) / BOUNDS) * DIMENSIONS as f32).floor() as i32;
+    // let z = (((pos.z + max).clamp(0., BOUNDS) / BOUNDS) * DIMENSIONS as f32).floor() as i32;
+
+
+    let x = ((pos.x - BOUNDS[0].x) / (BOUNDS[1].x - BOUNDS[0].x)).clamp(0., 1.0);
+    let y = ((pos.y - BOUNDS[0].y) / (BOUNDS[1].y - BOUNDS[0].y)).clamp(0., 1.0);
+    let z = ((pos.z - BOUNDS[0].z) / (BOUNDS[1].z - BOUNDS[0].z)).clamp(0., 1.0);
+
+    let x_i = (x * (DIMENSIONS[0] as f32 - 1.0)).floor() as i32;
+    let y_i = (y * (DIMENSIONS[1] as f32 - 1.0)).floor() as i32;
+    let z_i = (z * (DIMENSIONS[2] as f32 - 1.0)).floor() as i32;
+
+    return (x_i, y_i, z_i);
 }
 
 // fn avoid_nearby (
@@ -143,33 +157,43 @@ fn get_cell_index (pos: Vec3) -> (i32, i32, i32) {
 
 fn steer_towards_average_local_velocity (
     mut query: Query<&mut Velocity>,
+    mut grid: ResMut<GridMap>
 ) {
 
 }
 
 fn stay_inside_bounds (
-    mut boid_query: Query<&mut Transform, With<Boid>>,
-) {    
-    for mut transform in boid_query.iter_mut() {
-        if transform.translation.x > (BOUNDS/2.) {
-            transform.translation.x = -(BOUNDS/2.);
+    mut boid_query: Query<(&mut Transform, Entity), With<Boid>>,
+    mut grid: ResMut<GridMap>
+) {
+    let mut new_pos = Vec3::ZERO;
+    
+    for (mut transform, entity) in boid_query.iter_mut() {
+        let old_pos = transform.translation;
+
+        if transform.translation.x > BOUNDS[1].x {
+            new_pos.x = BOUNDS[0].x;
         }
-        else if transform.translation.x < -(BOUNDS/2.) {
-            transform.translation.x = (BOUNDS/2.);
+        else if transform.translation.x < BOUNDS[0].x {
+            new_pos.x = BOUNDS[1].x;
         }
 
-        if transform.translation.y > (BOUNDS/2.) {
-            transform.translation.y = -(BOUNDS/2.);
+        if transform.translation.y > BOUNDS[1].y {
+            new_pos.y = BOUNDS[0].y;
         }
-        else if transform.translation.y < -(BOUNDS/2.) {
-            transform.translation.y = (BOUNDS/2.);
+        else if transform.translation.y < BOUNDS[0].y {
+            new_pos.y = BOUNDS[1].y;
         }
 
-        if transform.translation.z > (BOUNDS/2.) {
-            transform.translation.z = -(BOUNDS/2.);
+        if transform.translation.z > BOUNDS[1].z {
+            new_pos.z = BOUNDS[0].z;
         }
-        else if transform.translation.z < -(BOUNDS/2.) {
-            transform.translation.z = (BOUNDS/2.);
+        else if transform.translation.z < BOUNDS[0].z {
+            new_pos.z = BOUNDS[1].z;
         }
+
+        
+        transform.translation = new_pos;
+        update_grid(old_pos, new_pos, &mut grid, entity);
     }
 }
